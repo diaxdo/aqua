b396beb05c333784c6b6d98e5cf2e604a0b5685d
jaebooker <jaesonbooker@gmail.com>
2017-03-11 17:27:27 -0800

    forked

--- aqua_game/aqua_game/GameScene.swift
+++ aqua_game/aqua_game/GameScene.swift
@@ -11,78 +11,33 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var violetnoodle = SKSpriteNode()
+    var pink_noodle = SKSpriteNode()
+    var green_noodle = SKSpriteNode()
+    var yring = SKSpriteNode()
+    var bring = SKSpriteNode()
+    var pring = SKSpriteNode()
+    var gring = SKSpriteNode()
+    var yring2 = SKSpriteNode()
+    var yring3 = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        violetnoodle = self.childNode(withName: "violetnoodle") as! SKSpriteNode
+        pink_noodle = self.childNode(withName: "pink_noodle") as! SKSpriteNode
+        green_noodle = self.childNode(withName: "green_noodle") as! SKSpriteNode
+        yring = self.childNode(withName: "yring") as! SKSpriteNode
+        bring = self.childNode(withName: "bring") as! SKSpriteNode
+        gring = self.childNode(withName: "gring") as! SKSpriteNode
+        pring = self.childNode(withName: "pring") as! SKSpriteNode
+        yring2 = self.childNode(withName: "yring2") as! SKSpriteNode
+        yring3 = self.childNode(withName: "yring3") as! SKSpriteNode
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 1
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        self.physicsBody = border
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
